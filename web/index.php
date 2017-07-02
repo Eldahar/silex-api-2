6b2014265cc51eac04479fd57cfb6f520aac8afa
Tibor Oravecz <tibor.oravecz@gwshub.com>
2017-07-02 11:47:37 +0200

    First commit

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// ... definitions
+$app->get('/api/second', function() {
+    return new \Symfony\Component\HttpFoundation\Response('Second api response!');
+});
+
+$app->run();
